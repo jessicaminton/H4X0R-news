4a53e88fa6af4a32d5bd2c5daea1aba208e3122e
Jessica Minton <jessminton2@gmail.com>
2022-09-28 12:53:00 -0400

    Initial Commit

---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by Jessica Minton on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
